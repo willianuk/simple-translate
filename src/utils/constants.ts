2567cb6a94abb0dda03d83412011b88bbb0eaf93
will <codeqrt8273@gmail.com>
2026-04-01 12:41:23 -0500

    fix: adjust popup layout and swap default languages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 export const LANGUAGES = [
-  { code: "spa", name: "Spanish" },
-  { code: "eng", name: "English" },
-  { code: "fra", name: "French" },
-  { code: "ger", name: "German" },
-  { code: "ita", name: "Italian" },
-  { code: "por", name: "Portuguese" }
+    { code: "spa", name: "Spanish" },
+    { code: "eng", name: "English" },
+    { code: "fra", name: "French" },
+    { code: "ger", name: "German" },
+    { code: "ita", name: "Italian" },
+    { code: "por", name: "Portuguese" }
 ]
 
 export const MAX_TEXT_LENGTH = 500
 export const API_ENDPOINT = "https://api.reverso.net/translate/v1/translation"
-export const DEFAULT_FROM_LANG = "spa"
-export const DEFAULT_TO_LANG = "eng"
+export const DEFAULT_FROM_LANG = "eng"
+export const DEFAULT_TO_LANG = "spa"
