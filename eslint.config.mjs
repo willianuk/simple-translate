e66d627edc869075b3e877b02e78afc94bd3a387
will <codeqrt8273@gmail.com>
2026-04-24 11:48:12 -0500

    ci(github): implement automated release and PR workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from "eslint/config"
 import tseslint from "typescript-eslint"
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended
+    eslint.configs.recommended,
+    tseslint.configs.recommended
 )
